3198984de0cac6ff4e08a0a3da7a101d7facc74b
powenko <powenko@gmail.com>
2014-05-02 05:40:23 +0000

    update weather lcd code

--- 03YahooWeatherOnLCD/YahooWeatherOnLCD.py
+++ 03YahooWeatherOnLCD/YahooWeatherOnLCD.py
@@ -1,7 +1,9 @@
+
 #!/usr/bin/python
 # create by Powen Ko, www.powenko.com
 import sys, getopt
 import urllib
+import os
 from xml.dom import minidom
 
 WEATHER_URL = 'http://xml.weather.yahoo.com/forecastrss?u=c&w=%s'
@@ -11,18 +13,20 @@ def weather_for_zip(zip_code):
     url = WEATHER_URL % zip_code
     dom = minidom.parse(urllib.urlopen(url))
     forecasts = ""
-    for node in dom.getElementsByTagNameNS(WEATHER_NS, 'forecast'):
-        forecasts.append({
-                         'L': node.getAttribute('low'),
-                         'H': node.getAttribute('high')
-                         })
     ycondition = dom.getElementsByTagNameNS(WEATHER_NS, 'condition')[0]
-    return {
-        'Now': ycondition.getAttribute('text'),
-        'C': ycondition.getAttribute('temp'),
-        'forecasts': forecasts
-    }
+    n= ycondition.getAttribute('text')
+    t= ycondition.getAttribute('temp')
+    return n+" "+t+"C"
+     
+
+
 zipcode=2436704
-if(len(sys.argv)>0):
+if(len(sys.argv)>1):
     zipcode=sys.argv[1]
-print weather_for_zip(zipcode)
+
+str1=weather_for_zip(zipcode)
+print str1
+str2='python ./lcdv2.py '
+newstr=str2+"'"+str1+"'"
+print newstr
+os.system(newstr)
